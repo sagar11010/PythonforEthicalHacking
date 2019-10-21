97c8c3be8ef283e7cdda0620bea294e733a09e0c
sagar11010 <sagarlpatel12@gmail.com>
2019-10-21 15:05:49 +0530

    code commenting

--- whatsappbom.py
+++ whatsappbom.py
@@ -1,8 +1,9 @@
+//Header files
 import pyautogui
 import time
 
 pyautogui.hotkey("alt","tab")
-time.sleep(1)
+time.sleep(1)//this statement is used for wait 1 seconds
 while True:
     pyautogui.typewrite("Hi bhaiya")
-    pyautogui.press("enter")
+    pyautogui.press("enter")//button pressing event
